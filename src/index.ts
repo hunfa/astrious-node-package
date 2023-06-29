a2adce8ea4dde1a3d705466de571e024f95d89a6
muhammad hunfa jalil <hunfa.jalil786@gmail.com>
2023-06-29 09:09:34 +0500

    new version

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,13 @@
 import axios, { AxiosResponse } from "axios"
 import { config } from "./config.js"
 
-interface CreateTransferPayload {
-    ip: string,
-    email: string,
-    email_domain: string,
-    session_id: string,
-    session: string,
-    public_token_object: {
-        public_token: string,
-    },
-    account_id: string,
-    customerUrl: string,
-    paymentId: string,
-    type: string,
-    apiDeckPayload: any,
-    user: string
-}
 
 interface CardPaymentPayload {
-    ip: string,
-    email: string,
-    email_domain: string,
-    session_id: string,
-    session: string,
+   
     token: string,
     zip: string | number,
     amount: string | number,
-    paymentId: string,
-    type: string,
-    apiDeckPayload: any,
-    user: string
+    
 }
 
 interface contructorPayload {
@@ -47,21 +24,7 @@ class Astrious {
         this.apiKey = payload.apiKey;
     }
 
-    async createTransfer(payload: CreateTransferPayload): Promise<AxiosResponse<string, any>> {
-
-
-        const credentials = `${this.appId}:${this.apiKey}`;
-        const encodedCredentials = btoa(credentials);
-
-        return await axios({
-            method: "POST",
-            url: `${config.endpoint}/publicCreateTransfer`,
-            headers: {
-                'authorization': `Basic ${encodedCredentials}`
-            },
-            data: payload
-        })
-    }
+ 
 
     async cardProcess(payload: CardPaymentPayload): Promise<AxiosResponse<string, any>> {
 
